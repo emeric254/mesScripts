ae3be75b788296d72838fd79c188da351e3c84ff
Tosi Émeric <emeric254@users.noreply.github.com>
2017-11-29 12:15:52 +0100

    Create detect_cpu_arch.sh

---
+++ system/detect_cpu_arch.sh
@@ -0,0 +1,14 @@
+ARCH=$(uname -m)
+case $ARCH in
+x86_64)
+    ARCH=x64  # or AMD64 or Intel64 or whatever
+    ;;
+i*86)
+    ARCH=x86  # or IA32 or Intel32 or whatever
+    ;;
+*)
+    # leave ARCH as-is
+    ;;
+esac
+
+echo $ARCH
